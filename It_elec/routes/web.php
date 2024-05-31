cc539d89b78b0c7526ee1375a05f0503694860ea
alangabriel45 <139440268+alangabriel45@users.noreply.github.com>
2024-05-31 09:47:58 +0800

    update web.php

--- It_elec/routes/web.php
+++ It_elec/routes/web.php
@@ -28,7 +28,7 @@ Route::get('/productdetails/{id}', [ProductController::class,'productdetails'])-
 Route::post('/transaction/{id}', [ProductController::class,'transaction'])->middleware('auth');
 Route::get('/viewshop/{id}', [PageController::class,'viewshop'])->middleware('auth');
 Route::post('/addtocart/{id}', [ProductController::class,'addtocart'])->middleware('auth');
-Route::get('/viewcart', [PageController::class, 'viewcart'])->name('viewcart');
-Route::post('/updatecartitem/{id}', [PageController::class, 'updateCartItemQuantity']);
-Route::delete('/removecartitem/{id}', [PageController::class, 'removeCartItem']);
-Route::get('/checkout', [PageController::class, 'index']);
+Route::get('/viewcart', [PageController::class, 'viewcart'])->middleware('auth');
+Route::post('/updatecartitem/{id}', [PageController::class, 'updateCartItemQuantity'])->middleware('auth');
+Route::delete('/removecartitem/{id}', [PageController::class, 'removeCartItem'])->middleware('auth');
+Route::get('/checkout', [PageController::class, 'index'])->middleware('auth');
